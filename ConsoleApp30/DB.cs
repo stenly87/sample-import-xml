345575df18ce56a0e352ebf9f55b1276b6f04fb6
ppk <ppk_student@hotmail.com>
2022-02-10 10:01:52 +1000

    Add project files.

---
+++ ConsoleApp30/DB.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp30
+{
+    internal class DB : DbContext
+    {
+        public DbSet<record> BloodServices { get; set; }
+
+        public DB()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            SqlConnectionStringBuilder stringBuilder = new SqlConnectionStringBuilder();
+            stringBuilder.DataSource = @"192.168.1.14\sqlexpress";
+            stringBuilder.UserID = "student";
+            stringBuilder.Password = "student";
+            stringBuilder.InitialCatalog = "1135_blood";
+            optionsBuilder.UseSqlServer(stringBuilder.ToString());
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
---
+++ ConsoleApp30/Program.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace ConsoleApp30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DB db = new DB();
+            
+            string path = @"C:\Users\user\Desktop\КЗ_РЧ20_21_Основная группа\Session 1\blood_services.xml";
+            XmlSerializer xml = new XmlSerializer(typeof(List<record>));
+            List<record> result = null;
+            using (var fs = File.OpenRead(path))
+                result = (List<record>)xml.Deserialize(fs);
+            Console.WriteLine(result[0].finishedDT.ToShortDateString());
+            
+            db.BloodServices.AddRange(result);
+            using (var transaction = db.Database.BeginTransaction())
+            {
+                db.Database.ExecuteSqlRaw("set identity_insert [1135_blood].[dbo].[BloodServices] ON;");
+                db.SaveChanges();
+                db.Database.ExecuteSqlRaw("set identity_insert [1135_blood].[dbo].[BloodServices] OFF;");
+                transaction.Commit();
+            }
+        }
+    }
+
+    public class record
+    { 
+        [Key]
+        
+        public int blood { get; set; }
+        public int service { get; set; }
+        public double result { get; set; }
+        public long finished { get; set; }
+        public bool accepted { get; set; }
+        public string status { get; set; }
+        public string analyzer { get; set; }
+        public int user { get; set; }
+
+        public DateTime finishedDT { get =>
+                DateTimeOffset.FromUnixTimeMilliseconds(finished).DateTime;
+        }
+    }
+
+    /*public class record
+    {
+        public int id { get; set;}
+        public int patient { get; set; }
+        public long date { get; set; }
+        public int barcode { get; set; }
+    }*/
+}
